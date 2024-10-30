14c89fe3869f467fe50c0b781ac31bcce6a5e768
Macmini <sseregit@knowwheresoft.com>
2024-10-30 16:46:09 +0900

    settings

---
+++ src/test/java/dev/allinone/libraryapp/LibraryappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.allinone.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

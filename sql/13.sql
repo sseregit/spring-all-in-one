d4153fa6a2a6967d6eb06c01fb72e0098d5e92dd
Macmini <sseregit@knowwheresoft.com>
2024-10-31 15:40:51 +0900

    Spring에서 Database 사용하기

---
+++ sql/13.sql
@@ -0,0 +1,10 @@
+use library;
+
+create table user (
+    id bigint auto_increment,
+    name varchar(25),
+    age int,
+    primary key (id)
+);
+
+show tables;
--- src/main/java/dev/allinone/libraryapp/controller/user/UserController.java
+++ src/main/java/dev/allinone/libraryapp/controller/user/UserController.java
@@ -1,36 +1,45 @@
 package dev.allinone.libraryapp.controller.user;
 
-import dev.allinone.libraryapp.domain.user.User;
 import dev.allinone.libraryapp.dto.user.request.UserCreateRequest;
 import dev.allinone.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 class UserController {
 
-    private final List<User> users = new CopyOnWriteArrayList<>();
+    private final JdbcClient jdbcClient;
+
+    public UserController(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
 
     @PostMapping("/user")
     void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.name(), request.age()));
+        jdbcClient.sql("""
+                        insert into user (name, age) values (:name, :age);
+                        """)
+                .param("name", request.name())
+                .param("age", request.age())
+                .update();
     }
 
     @GetMapping("/user")
     List<UserResponse> getUsers() {
-        List<UserResponse> reponses = new ArrayList<>();
-
-        for (int i = 0; i < users.size(); i++) {
-            reponses.add(new UserResponse((long) (i + 1), users.get(i)));
-        }
-
-        return reponses;
+        return jdbcClient.sql("""
+                        select * from user;
+                        """)
+                .query((rs, rowNum) -> {
+                    Long id = rs.getLong("id");
+                    String name = rs.getString("name");
+                    Integer age = rs.getInt("age");
+                    return new UserResponse(id, name, age);
+                }).list();
     }
 
 

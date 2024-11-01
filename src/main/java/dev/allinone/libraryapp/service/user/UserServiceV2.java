b8e938f0184c027893da99ad7a173c98ff58ad32
Macmini <sseregit@knowwheresoft.com>
2024-11-01 17:13:08 +0900

    트랜잭션 적용과 영속성 컨텍스트

--- src/main/java/dev/allinone/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/dev/allinone/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import dev.allinone.libraryapp.dto.user.request.UserCreateRequest;
 import dev.allinone.libraryapp.dto.user.request.UserUpdateRequest;
 import dev.allinone.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,10 +19,12 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         User u = userRepository.save(new User(request.name(), request.age()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         return userRepository.findAll()
                 .stream()
@@ -29,15 +32,15 @@ public class UserServiceV2 {
                 .toList();
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.id())
                 .orElseThrow(IllegalArgumentException::new);
 
         user.updateName(request.name());
-
-        userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name)
                 .orElseThrow(IllegalArgumentException::new);
